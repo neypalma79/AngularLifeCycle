1334503d2a048395320b4a27c39bf44fec36011e
Ney Palma <ney_palma@yahoo.com>
2023-07-20 16:34:25 -0500

    metodos ciclo de vida, ng-container, ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent
+  implements OnChanges, OnInit, AfterViewInit, OnDestroy
+{
   title = 'AngularLifeCycle';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.info('OnChanges');
+  }
+
+  ngOnInit(): void {
+    console.info('Init');
+  }
+
+  ngAfterViewInit(): void {
+    console.info('AfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    console.info('Destroy');
+  }
 }
---
+++ src/app/lista.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css'],
+})
+export class ListaComponent {
+  @Input()
+  public color: string = '';
+}
